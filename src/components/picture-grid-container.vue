ff020ae81eae983242125a71eadea769f23d1317
colby-moore <colbym2014@gmail.com>
2019-06-17 21:28:16 -0400

    Initial commit

---
+++ src/components/picture-grid-container.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-container fluid class="picture-grid-container">
+        <b-row>
+            <pictureItem />
+            <pictureItem />
+            <pictureItem />
+            <pictureItem />
+            <pictureItem />
+            <pictureItem />
+            <pictureItem />
+            <pictureItem />
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import pictureItem from '../components/picture-item.vue'
+
+export default {
+    name: 'pictureGridContainer',
+      components: {
+        pictureItem,
+    }
+}
+</script>
+
+<style>
+    .picture-grid-container{
+        background-color: gray;
+        min-height: 100vh;
+    }
+</style>
---
+++ src/components/picture-item.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-col sm="12" md="4" lg="3" class="picture-item">
+    <img src="https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=650&w=940" />
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: 'pictureItem',
+}
+</script>
+
+<style scoped>
+  .picture-item{
+    margin-bottom: 40px;
+  }
+  .picture-item img{
+    width: 100%;
+  }
+</style>
